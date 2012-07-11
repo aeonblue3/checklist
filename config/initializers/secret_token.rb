e0dd706e0768adbff4ac99dfe45fee7c6d81383a
Chris Neitzer <chrisn@soholaunch.com>
2012-07-11 14:20:43 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checklist::Application.config.secret_token = '4d90f692422ed38e7850a8bf7f879c1031eb9a5efcb88f518c75c90d7554dd18f2d22b13898e3a52b67a6c962e4eb6d88c1c2f029b3dd959a18fb9b005081eac'
