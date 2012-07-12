465c372ac3b54e0a429b508d6b259de4a895a271
Chris Neitzer <chrisn@soholaunch.com>
2012-07-12 15:32:55 -0400

    Removed features and step definitions

--- features/create_task.feature
+++
@@ -1,6 +0,0 @@
-Feature: Creating a task
-
-	Scenario: A user can create a task
-
-		When a user creates a task
-		Then it should be saved in the database
--- features/step_definitions/create_task_steps.rb
+++
@@ -1,7 +0,0 @@
-When /^a user creates a task$/ do
-  @todo = Task.new({:name => 'do stuff', :desc => 'Make sure it gets done'})
-end
-
-Then /^it should be saved in the database$/ do
-  @todo.save
-end
